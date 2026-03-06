f446607b2e8a1b007d33d9ff6cf6da8a7121cfc0
RobertOrkwiszewski <161032536+RobertOrkwiszewski@users.noreply.github.com>
2026-03-06 15:41:37 +0100

    Add files via upload

--- backend.js
+++ backend.js
@@ -472,12 +472,16 @@ function displayBooks() {
                         // epub.js selected-Event als zusätzlicher Fallback
                         currentEpubRendition.on("selected", (cfiRange) => {
                             currentEpubBook.getRange(cfiRange).then((range) => {
-                                console.log("selected");
+                                /*console.log("selected");
                                 const text = range.toString().trim();
                                 if (text) {
                                     handleSelection(text, cfiRange);
-                                }
+                                }*/
+                                selectedText = range.toString().trim();
                             });
+                        });
+                        currentEpubRendition.on("touchend", (e) => {
+                            handleSelection(selectedText, selectedBook.currentPage);
                         });
 
                     };
